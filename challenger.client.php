7befb13096b0de591746bd314be5b46e6cf93f29
Tomas Stan <tomastan@enga.systems>
2022-07-08 14:18:33 +0300

    Remove redundant referrer header from API call

--- challenger.client.php
+++ challenger.client.php
@@ -144,7 +144,6 @@ class Challenger{
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_REFERER, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000); // 3 sec.
 		curl_setopt($ch, CURLOPT_TIMEOUT, 10000); // 10 sec.
